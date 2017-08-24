57e28f2791832d1ff0f4a07d74c7e02377aa213e
wesmail <waleed.esmail@cern.ch>
2017-08-24 11:59:06 +0200

    Create muStarmu2muZ_Lambda10TeV_M-1000_13TeV_pythia8_cff.py

---
+++ python/ThirteenTeV/muStarmu2muZ_Lambda10TeV_M-1000_13TeV_pythia8_cff.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         comEnergy = cms.double(13000.0),
+                         crossSection = cms.untracked.double(6.44),
+                         filterEfficiency = cms.untracked.double(1),
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'Tune: pp 5',
+            'ExcitedFermion:qqbar2muStarmu = on',
+            'ExcitedFermion:Lambda = 10000',
+            '4000013:onMode = off',
+            '4000013:onIfMatch = 13 23',
+            '4000013:m0 = 1000',
+            '23:onMode = off',
+            '23:onIfMatch = 13 -13',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
